428b43d0bb11e80001454fe67678511cedf44e11
Antonin Barthélémy <antobarth@gmail.com>
2017-05-04 15:47:36 +0200

    hmm

--- working_env/script/tw_split_base.py
+++ working_env/script/tw_split_base.py
@@ -1,9 +1,16 @@
 """Script split TW."""
 import minerminor.mm_utils as mmu
 import os
+import argparse
+
+parser = argparse.ArgumentParser(prog="Split Tw base")
+
+parser.add_argument("-p", "--path_dir", default="base_test",
+                    help="Path of base dir")
+args = parser.parse_args()
 
 learning_base = []
-path_base = "base_tw_2"
+path_base = args.path_dir
 list_labels_dir = os.listdir(path_base)
 for dir_name in list_labels_dir:
     base = mmu.tw_split_base(path_base+"/"+dir_name)
